37417b3b9960c3d384fb621f86f6e60e50e10f45
Justin Lin <justinlin@Justins-MacBook-Pro-5.local>
2020-11-16 10:59:17 -0800

    Pushing framework of Vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <template>
   <div id="app">
-    <h1>Hello World</h1>
+    <span id="appContainer">
+      <div>
+        <h1 id="version">Version Number: 0.0.0</h1>
+      </div>
+      <button>Add User</button>
+      <button>Toggle Barcode / NetID</button>
+      <h1>Search Bar</h1>
+      <button>SEARCH USER</button>
+      <h1>NAME</h1>
+      <h1>NET ID</h1>
+      <h1>Barcode</h1>
+      <button>EDIT BARCODE BUTTON</button>
+      <button>EDIT</button><button>SAVE</button>
+      <h1>CONTAINER OF LISTS</h1>
+    </span>
   </div>
 </template>
 <script>
 export default {};
 </script>
-<style lang="scss">
-</style>
+<style scoped>
+#appContainer {
+  display: flex;
+  width: 75%;
+  margin: auto;
+  flex-direction: column;
+  align-items: center;
+}
+</style>>
