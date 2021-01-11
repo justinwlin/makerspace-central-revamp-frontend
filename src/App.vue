3518679940b2129503b52ee189cd8d6b4bafeb25
Justin Lin <justinlin@justins-mbp-5.lan>
2021-01-10 18:57:41 -0800

    Implementing add user view

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,19 @@
 <template>
   <div id="app">
-    <!-- NAVIGATION -->
-    <div class="navBar center">
-      <el-button type="primary">Home</el-button>
-      <el-button type="success">Add User</el-button>
-    </div>
+    <Home></Home>
 
-    <!-- SEARCH BOX -->
-    <el-row type="flex" justify="center">
-      <el-col :span="8">
-        <el-input placeholder="Please input" v-model="input"></el-input>
-      </el-col>
-      <el-button type="primary" plain>Search By NetID</el-button>
-    </el-row>
-    <!-- RESULTS OF SEARCH -->
-    <el-row type="flex" justify="center">
-      <el-col :span="12">
-        <h2>User:</h2>
-      </el-col>
-    </el-row>
-    <el-row type="flex" justify="center">
-      <el-col :span="12">
-        <div>
-          <h4>Edit Barcode</h4>
-          <el-input placeholder="Please input" v-model="input"></el-input>
-        </div>
-        <div>
-          <h4>Edit NetID</h4>
-          <el-input placeholder="Please input" v-model="input"></el-input>
-        </div>
-      </el-col>
-    </el-row>
-    <!-- GRID -->
-    <div class="center">
-      <el-button type="info" plain>EDIT</el-button>
-      <el-button type="success" plain>SAVE</el-button>
-    </div>
+    <About v-if="false"></About>
   </div>
 </template>
 
 <script>
+import Home from "./views/Home";
+import About from "./views/About";
 export default {
   name: "App",
-  components: {},
+  components: { Home, About },
 };
 </script>
 
 <style scoped>
-.center {
-  display: flex;
-  justify-content: center;
-  margin: 1rem;
-}
 </style>
